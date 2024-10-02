6d7e7fbd9eeab52a38c153be1431994a12ffadf3
adamjatim <adamrahmatilahi@gmail.com>
2024-10-02 03:41:24 -0300

    update: add not found 404 page

---
+++ resources/js/pages/NotFoundComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Ups... 404</h1>
+        <p>the is page is Not Found!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFoundComponent'
+}
+</script>
