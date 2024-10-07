e58d049ae3d1f3da6a29af4b97bab26b23bf2352
adamjatim <adamrahmatilahi@gmail.com>
2024-10-07 20:54:58 +0700

    update: giving condition for children route

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,13 +1,39 @@
 <template>
-  <nav>
-    <router-link v-bind:to="{ name: 'Home' }" class="nav-link">Home</router-link>
-    <router-link v-bind:to="{ name: 'About' }" class="nav-link">About</router-link>
-    <router-link v-bind:to="{ name: 'User' }" class="nav-link">User</router-link>
-  </nav>
+    <nav>
+        <router-link
+            v-bind:to="{ name: 'Home' }"
+            :class="isActive('Home') ? 'active' : ''"
+            class="nav-link"
+            >Home</router-link
+        >
+
+        <router-link
+            v-bind:to="{ name: 'About' }"
+            :class="isActive('About') ? 'active' : ''"
+            class="nav-link"
+            >About</router-link
+        >
+
+        <router-link
+            v-bind:to="{ name: 'User' }"
+            :class="isActive('User') || isActive('Profile') ? 'active' : ''"
+            class="nav-link"
+            >User</router-link
+        >
+    </nav>
 </template>
 
 <script>
 export default {
-  name: "HeaderComponent",
+    name: "HeaderComponent",
+    computed: {
+        // A method to check the currently active route
+        isActive() {
+            return (routeName) => {
+                // Check if the current route matches the given route name
+                return this.$route.name === routeName;
+            };
+        },
+    },
 };
 </script>
