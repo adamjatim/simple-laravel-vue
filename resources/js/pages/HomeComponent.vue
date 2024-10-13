9caeee783ed066423ec1cf2c74bebc19953718ed
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:09:52 +0700

    update HomeComponent.vue

--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the Home Page!</p>
-    </div>
+	<div>
+		<h1>Home Page</h1>
+		<p>Welcome to the Home Page!</p>
+	</div>
 </template>
 
 <script>
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 export default {
-    name: 'HomeComponent'
-}
+	name: "HomeComponent",
+	setup() {
+		// toast("welcome to my website");
+	},
+};
 </script>
