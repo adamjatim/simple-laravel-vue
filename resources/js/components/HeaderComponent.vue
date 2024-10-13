da28d23b97a04db2901f85c2395d1ccae9f4db85
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:09:46 +0700

    update HeaderComponent.vue

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,39 +1,43 @@
 <template>
-    <nav>
-        <router-link
-            v-bind:to="{ name: 'Home' }"
-            :class="isActive('Home') ? 'active' : ''"
-            class="nav-link"
-            >Home</router-link
-        >
+	<nav>
+		<router-link
+			v-bind:to="{ name: 'Home' }"
+			:class="isActive('Home') ? 'active' : ''"
+			class="nav-link"
+			>Home</router-link
+		>
 
-        <router-link
-            v-bind:to="{ name: 'About' }"
-            :class="isActive('About') ? 'active' : ''"
-            class="nav-link"
-            >About</router-link
-        >
+		<router-link
+			v-bind:to="{ name: 'About' }"
+			:class="isActive('About') ? 'active' : ''"
+			class="nav-link"
+			>About</router-link
+		>
 
-        <router-link
-            v-bind:to="{ name: 'User' }"
-            :class="isActive('User') || isActive('Profile') ? 'active' : ''"
-            class="nav-link"
-            >User</router-link
-        >
-    </nav>
+		<router-link
+			v-bind:to="{ name: 'User' }"
+			:class="
+				isActive('User') || isActive('RegisterUser') || isActive('Profile')
+					? 'active'
+					: ''
+			"
+			class="nav-link"
+			>User</router-link
+		>
+	</nav>
 </template>
 
 <script>
 export default {
-    name: "HeaderComponent",
-    computed: {
-        // A method to check the currently active route
-        isActive() {
-            return (routeName) => {
-                // Check if the current route matches the given route name
-                return this.$route.name === routeName;
-            };
-        },
-    },
+	name: "HeaderComponent",
+	computed: {
+		// A method to check the currently active route
+		isActive() {
+			return (routeName) => {
+				// Check if the current route matches the given route name
+				return this.$route.name === routeName;
+			};
+		},
+	},
 };
 </script>
