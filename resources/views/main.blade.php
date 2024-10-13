7bc3fcdd6e15d4b1843d0663735071785e03b1b2
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:10:19 +0700

    update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Laravel vue SPA</title>
-  @vite('resources/sass/app.scss')
-  <style>
-    body {
-      font-family: sans-serif;
-      margin: 15%;
-    }
-  </style>
-  @vite('resources/js/app.js')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel vue SPA</title>
+    @vite('resources/sass/app.scss')
+    <style>
+        body {
+            font-family: sans-serif;
+            margin: 15%;
+        }
+    </style>
+    @vite('resources/js/app.js')
 </head>
+
 <body>
-  <div id="app">
-    <h1 v-text="'Hello, ' +title"></h1>
-    <header-component></header-component>
-    <router-view></router-view>
-    <footer-component></footer-component>
-  </div>
+    <div id="app">
+        <h1 v-text="'Hello, ' +title"></h1>
+        <header-component></header-component>
+        <router-view></router-view>
+        <footer-component></footer-component>
+    </div>
 </body>
+
 </html>
