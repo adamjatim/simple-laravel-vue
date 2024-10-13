969b36a93e6ae044a511fddda00f779fb53c0acd
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:09:37 +0700

    update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,23 +11,23 @@ use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeders.
-     */
-    public function run(): void
-    {
-        for($i = 1; $i <=5; $i++) :
-            $faker = Faker::create();
-            // DB::table('users')->insert([
-            //     'name' => Str::random(10),
-            //     'email' => Str::random(10).'@example.com',
-            //     'password' => Hash::make('password'),
-            // ]);
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => Hash::make('rahasia'),
-            ]);
-        endfor;
-    }
+	/**
+	 * Run the database seeders.
+	 */
+	public function run(): void
+	{
+		for ($i = 1; $i <= 5; $i++) :
+			$faker = Faker::create();
+			// DB::table('users')->insert([
+			//     'name' => Str::random(10),
+			//     'email' => Str::random(10).'@example.com',
+			//     'password' => Hash::make('password'),
+			// ]);
+			DB::table('users')->insert([
+				'name' => $faker->name,
+				'email' => $faker->email,
+				'password' => Hash::make('rahasia'),
+			]);
+		endfor;
+	}
 }
