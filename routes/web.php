87442f66076185adc31cae7018588cf62fc9ff97
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:10:27 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-    return view('main');
+Route::get('/', function () {
+	return view('main');
 });
 
-// Route::any('/{slug}', function () {
-//     return view('main');
-// });
-
-// Route::any('/{slug}/{subSlug}', function () {
-//     return view('main');
-// });
-
-
-Route::get('/{any}', function() {
-    return view('main');
-})->where('any', '.*');
+Route::get('/{any}', function () {
+	return view('main');  // Return your Vue SPA entry point
+})->where('any', '.*');   // This handles all routes and serves the main view
