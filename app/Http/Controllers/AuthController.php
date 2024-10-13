24e4bc53dadd53303feead5f7068781bb8492620
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:09:24 +0700

    update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,20 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function index()
-    {
-        $users = DB::table('users')->select('id','name')->get();
-        // dd($users);
-        return response()->json($users);
-    }
+	public function index()
+	{
+		// $users = DB::table('users')->select('id','name')->get();
+		$users = User::all();
+		// dd($users);
+		return response()->json($users);
+	}
 
-    public function show($id)
-    {
-        $users = DB::table('users')->where('id', $id)->first();
-        // dd($users);
-        return response()->json($users);
-    }
+	public function show($id)
+	{
+		// $users = DB::table('users')->where('id', $id)->first();
+		$user = User::find($id);
+		// dd($users);
+		return response()->json($user);
+	}
+
+	public function store(Request $request)
+	{
+		$user = new User;
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->password = Hash::make($request->password);
+
+		$user->save();
+		return response()->json([
+			"status" => true,
+			"message" => 'User data successfully saved',
+			"data" => $user
+		]);
+	}
 }
