f6b1c75d08abffd0c35e2075dd317924a09dbc12
adamjatim <adamrahmatilahi@gmail.com>
2024-10-13 22:10:23 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/users', [AuthController::class, 'index' ]);
-Route::get('/users/{id}', [AuthController::class, 'show' ]);
+Route::get('/users', [AuthController::class, 'index']);
+Route::post('/users', [AuthController::class, 'store']);
+
+Route::get('/users/{id}', [AuthController::class, 'show']);
